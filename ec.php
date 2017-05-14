f266be7a2ea0be63b2a17aa7d15073229af5caba
Weida Chen <dada@Weidas-MacBook.local>
2017-05-14 23:21:00 +0800

    test check

---
+++ ec.php
@@ -0,0 +1,144 @@
+<?php
+class test extends PHPUnit_Framework_TestCase
+{
+  public function test_檢查出資料資料的格式_輸入cvs_回傳cvs()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_檢查出資料資料的格式_輸入tvs_回傳tvs()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_檢查出資料資料的格式_輸入atom_回傳atom()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_檢查出資料資料的格式_輸入xml_回傳xml()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_驗證用戶網址是否正確並回傳網址格式_輸入https_回傳https()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_驗證用戶網址是否正確並回傳網址格式_輸入http_回傳http()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_驗證用戶網址是否正確並回傳網址格式_輸入ftp_回傳ftp()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_驗證用戶網址是否正確並回傳網址格式_輸入sftp_回傳sftp()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_輸入ftp_帳號密碼_dada_123_回傳連線成功()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_輸入ftp_帳號密碼_weida_321_回傳連線失敗()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_檔案壓縮格式_輸入gz回傳gz()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_檔案壓縮格式_輸入zip回傳zip()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_檔案壓縮格式_輸入bz2回傳bz2()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_驗證資料數量_輸入10筆檔案1M回傳資料合法()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_驗證資料數量_輸入10筆但是檔案101M回傳資料不合法()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_驗證資料數量_輸入50萬零一筆回傳資料不合法()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_驗證資料編碼_輸入utf8資料回傳資料合法()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_驗證資料編碼_輸入big5資料回傳資料不合法()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_驗證cvs資料是否合法_欄位都有對應到而且必填欄位都有資料_回傳合法()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_驗證cvs資料是否合法_欄位沒有對應到但是必填欄位都有資料_回傳不合法()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_驗證cvs資料是否合法_必填欄位不完整_回傳不合法()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_資料欄位驗證_id_欄位()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  }
+
+  public function test_資料欄位驗證_title_欄位()
+  {
+      $this->markTestSkipped('skip.');
+    //$this->assertEquals($expected, $actual);
+  } 
+
+
+
+}
